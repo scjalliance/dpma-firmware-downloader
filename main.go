6238ffa1fd3ad94595f61981a106e19b0292f828
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-01-22 15:59:10 -0800

    Added MD5 caching for efficient detection of new and updated files

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/scjalliance/dpmafirmware"
@@ -48,4 +52,59 @@ func main() {
 
 	// Print the results for now
 	log.Printf("Manifest Data:\n%s", manifest.Summary())
+
+	for _, r := range manifest.Releases {
+		process(&config, &manifest.Origin, &r)
+	}
+}
+
+func process(config *Config, origin *dpmafirmware.Origin, r *dpmafirmware.Release) {
+	source := r.URL(origin)
+	cacheFile := config.CacheFile(r.Version)
+	cachedMD5Sum, err := readCache(cacheFile)
+
+	prefix := fmt.Sprintf("%-7s: ", r.Version)
+
+	if err == nil {
+		if cachedMD5Sum == r.MD5Sum {
+			log.Printf("%sUp to date (md5: %s)", prefix, r.MD5Sum)
+			return
+		} else {
+			log.Printf("%sRevision detected (md5: %s -> %s). Downloading from %s", prefix, cachedMD5Sum, r.MD5Sum, source)
+		}
+	} else if os.IsNotExist(err) {
+		log.Printf("%sMissing (md5: %s). Downloading from %s", prefix, r.MD5Sum, source)
+	} else {
+		log.Printf("%sFailed to read MD5 from %s: %v", prefix, filepath.Base(cacheFile), err)
+		return
+	}
+
+	err = download(source, config.FirmwareDir)
+	if err != nil {
+		log.Printf("%sDownload failed: %v", prefix, err)
+		return
+	}
+
+	err = writeCacheFile(cacheFile, r.MD5Sum)
+	if err != nil {
+		log.Printf("%sFailed to write \"%s\" to cache file: %v", prefix, r.MD5Sum, err)
+		return
+	}
+}
+
+func download(source *url.URL, destDir string) error {
+	log.Printf("Downloading from %s to %s", source, destDir)
+	return nil
+}
+
+func readCache(path string) (string, error) {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
+
+func writeCacheFile(path string, md5sum string) error {
+	return ioutil.WriteFile(path, []byte(md5sum), 0644)
 }
